eba96911034a33a803617e7b2c2a93887f33df7d
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-09 20:54:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(arrays){
   if(array.length == 1){
     return "I am 1 strange loop";
   } else{
-    var newArray = []];
+    var newArray = [];
     for (var i = 1; i < arrays.length; i++){
       newArray.push(`I am ${i} strange loops.`);
     }
